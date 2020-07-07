6d323f5f416152b31b9186d6a6bcd1b050c5333d
Jasmine Epps <epps.jasmine@gmail.com>
2020-07-06 23:29:20 -0400

    updated client directories

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.use("/api", apiRoutes);
 // });
 
 router.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
 module.exports = router;
