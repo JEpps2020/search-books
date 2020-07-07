41ad16a516a786e18e55be2cc4694baa67fbbe04
Jasmine Epps <epps.jasmine@gmail.com>
2020-07-07 00:17:30 -0400

    commented out route in index.js under routes

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,8 @@ router.use("/api", apiRoutes);
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
 
-router.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
+// router.use(function (req, res) {
+//   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+// });
 
 module.exports = router;
